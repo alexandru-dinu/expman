f7b1d469b7e7ea4639973f33ce5e5759ed9c9d4b
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-24 15:54:06 +0300

    Create fault_tolerant_dataloader.py

---
+++ fault_tolerant_dataloader.py
@@ -0,0 +1,41 @@
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.data._utils.collate import default_collate
+
+
+class CustomDataset(Dataset):
+    def __init__(self):
+        # data init
+        ...
+
+    def __getitem__(self, i):
+        try:
+            # unreliable data source
+            # processing
+            ...
+            return self.data[i]
+        except (FileNotFoundError, ...):
+            return None
+
+    def __len__(self):
+        return len(self.data)
+
+    @staticmethod
+    def collate(xs):
+        good = [x for x in xs if x is not None]
+
+        if len(good) == 0:
+            return None
+
+        return default_collate(good)
+
+    
+dataset = CustomDataset(...)
+dataloader = DataLoader(dataset, collate_fn=CustomDataset.collate, ...)
+      
+      
+for idx, batch in enumerate(dataloader):
+    if batch is None:
+        continue
+    # unpack batch
+    x, y, z = batch
+    ...
