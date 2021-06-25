d5b3f7a3be577670739c927527feaa5ec848e202
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-25 16:07:48 +0300

    Linting.

--- src/data_split.py
+++ src/data_split.py
@@ -5,7 +5,7 @@ from typing import List, Any, Dict
 def split_data(data: List[Any], train_f: float, test_f: float) -> Dict[str, List[Any]]:
     n = len(data)
     gen = (x for x in data)  # to keep offset internally
-    
+
     return {
         "train": list(take(int(n * train_f), gen)),  # take first
         "test": list(take(int(n * test_f), gen)),  # take next
--- src/processing_template.py
+++ src/processing_template.py
@@ -1,14 +1,14 @@
 def main() -> None:
-  # use args
-  # use logging
-  ...
+    # use args
+    # use logging
+    ...
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     ...
     args = parser.parse_args()
-    
+
     logger = setup_logging(...)
-    
+
     main()
