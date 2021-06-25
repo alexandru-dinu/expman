e726f91d29f1da5d65970a1cf1936045354e047d
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-25 15:36:03 +0300

    Create processing_template.py

---
+++ src/processing_template.py
@@ -0,0 +1,14 @@
+def main() -> None:
+  # use args
+  # use logging
+  ...
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    ...
+    args = parser.parse_args()
+    
+    logger = setup_logging(...)
+    
+    main()
