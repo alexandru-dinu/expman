f03961def34c2f9e208e9aece2e96a9aff9e195f
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-28 13:46:28 +0300

    Create viz.py

---
+++ src/viz.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def stem_hist(xs: np.ndarray, show_now=True, linefmt="C0-", markerfmt="C0o") -> None:
+    x, f = np.unique(xs, return_counts=True)
+    f = f / f.sum()
+
+    plt.stem(x, f, use_line_collection=True, linefmt=linefmt, markerfmt=markerfmt)
+    q = max(1, x.size // 10)
+    plt.xticks(x[::q])
+
+    if show_now:
+        plt.show()
