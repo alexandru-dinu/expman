c8b53ecceff8d02e2be1f2a750804821f6eadc34
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-28 13:45:50 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from toolz.itertoolz import take
 from typing import *
 
@@ -31,3 +33,21 @@ def split_data(
         "test": list(take(int(n * test_f), gen)),  # take next
         "valid": list(gen),  # take remaining
     }
+
+
+def batchify(xs: np.ndarray, bs: int, func: callable = None):
+    """
+    Batchify @xs. If @func is not None, then the emitted item is func(batch).
+    """
+    if not isinstance(bs, int) or not (1 <= bs <= len(xs)):
+        raise ValueError(f"Batch size must be an int in [1, {xs.shape[0]}].")
+
+    if not isinstance(xs, np.ndarray):
+        xs = np.array(xs)
+
+    if func is None:
+        func = lambda x: x
+
+    l = len(xs)
+    for ndx in range(0, l, bs):
+        yield func(xs[ndx : min(ndx + bs, l)])
