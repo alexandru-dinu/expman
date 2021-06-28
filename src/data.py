582ec4c3a5b69e76df63371a61dab3f21f16931e
Alexandru Dinu <alex.dinu07@gmail.com>
2021-06-28 13:31:40 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -12,9 +12,19 @@ def seed_everything(seed: Optional[int]) -> None:
     T.manual_seed(seed)
 
 
-def split_data(data: List[Any], train_f: float, test_f: float) -> Dict[str, List[Any]]:
+def split_data(
+    data: List[Any], train_f: float, test_f: float, shuffle: bool = False
+) -> Dict[str, List[Any]]:
+    """Get train / test / valid splits from `data`.
+    If `shuffle` is True, then use a random permutation of `data`."""
     n = len(data)
-    gen = (x for x in data)  # to keep offset internally
+
+    # use a generator to keep offset internally when taking elements
+    if shuffle:
+        rand_idx = np.random.permutation(n)
+        gen = (data[i] for i in rand_idx)
+    else:
+        gen = (x for x in data)
 
     return {
         "train": list(take(int(n * train_f), gen)),  # take first
