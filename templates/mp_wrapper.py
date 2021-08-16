c1c5ac981b3a1fdae39e8f3ac53d4f786424ddcd
Alexandru Dinu <alex.dinu07@gmail.com>
2021-08-16 11:24:43 +0300

    Create mp_wrapper.py

---
+++ templates/mp_wrapper.py
@@ -0,0 +1,37 @@
+def processing_func(x, data, foo, bar) -> OutputType:
+    """
+    This function does not know anything about CLI args.
+    """
+    ...
+    return out
+
+
+def _wrapper(x, **kwargs):
+    """
+    This wrapper may use CLI args.
+    """
+    foo = kwargs.get("foo")
+    bar = kwargs.get("bar")
+  
+    try:
+        data = load(args.input_file_path)
+        out = processing_func(x, data, foo, bar)
+    except:
+        logger.warning(...)
+        out = {}
+    
+    return x, out
+
+
+def main():
+    # work_fn :: type(x) -> (type(x), OutputType)
+    work_fn = functools.partial(
+        _wrapper,
+        foo=foo,
+        bar=bar,
+    )
+    work_args = [x for x in ...]
+    
+    with mp.Pool(num_processes) as pool:
+        for x, out in pool.imap_unordered(work_fn, work_args):
+            ...
