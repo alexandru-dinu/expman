1e4002fa65707d8ec185a47296064a5084872e54
Alexandru Dinu <alex.dinu07@gmail.com>
2021-10-01 11:17:56 +0300

    Docs: separate reference / template.

--- docs/gen_references.py
+++ docs/gen_references.py
@@ -4,16 +4,20 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 src_root = root / "src"
 ref_root = root / "docs/sources/reference"
+tpl_root = root / "docs/sources/template"
 
-if ref_root.exists():
-    shutil.rmtree(ref_root)
-
-ref_root.mkdir(exist_ok=True)
+for p in [ref_root, tpl_root]:
+    if p.exists():
+        shutil.rmtree(p)
+    p.mkdir(exist_ok=True)
 
 for script in src_root.glob("*.py"):
-    ref = (ref_root / script.stem).with_suffix(".md")
+    if script.stem.startswith("_"):
+        path = (tpl_root / script.stem).with_suffix(".md")
+    else:
+        path = (ref_root / script.stem).with_suffix(".md")
 
-    with open(ref, "wt") as fp:
+    with open(path, "wt") as fp:
         fp.write(f"::: src.{script.stem}\n")
 
-print(f"Generated references to {ref_root}.")
+    print(f"> generated ref to {path}.")
